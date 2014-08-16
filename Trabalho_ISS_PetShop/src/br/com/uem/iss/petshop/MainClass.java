e83be1380ebfbba08ca876faa0d24f5a47258038
eduardo <eduardovillasboas@outlook.com.br>
2014-08-16 19:07:22 -0300

    classe de inicializacao do sistema

---
+++ Trabalho_ISS_PetShop/src/br/com/uem/iss/petshop/MainClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.uem.iss.petshop;
+
+/**
+ *
+ * @author EDUARDO
+ * classe principal do projeto a partir dessa classe a aplicacação é
+ * iniciada
+ */
+public class MainClass {
+    
+    public static void main(String args[]) {
+        
+    }
+}
